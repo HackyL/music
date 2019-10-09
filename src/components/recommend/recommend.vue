2735d45879dd8b459991b04b94dc0bd4b417bd85
李阳 <11103871@vivo.xyz>
2019-10-09 16:13:23 +0800

    feat: hhh

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,36 +1,52 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div>
-      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-        <slider>
-          <div v-for="(item,index) in recommends" :key="index">
-            <a :href="item.linkUrl">
-              <img class="needsclick" :src="item.picUrl">
-            </a>
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div>
+          <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+            <div class="slider-content">
+              <slider ref="slider">
+                <div v-for="(item,index) in recommends" :key="index">
+                  <a :href="item.linkUrl">
+                    <img @load="loadImage" class="needsclick" :src="item.picUrl">
+                  </a>
+                </div>
+              </slider>
+            </div>
           </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>          
-        </ul>
-      </div>
-    </div>
+          <div class="recommend-list">
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul>
+              <li class="item" v-for="(item,index) in discList" :key="index">
+                <div class="icon">
+                  <img v-lazy="item.imgurl" width="60" height="60">
+                </div>
+                <div class="text">
+                  <h2 class="name" v-html="item.creator.name"></h2>
+                  <p class="desc" v-html="item.dissname"></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
+      </scroll> 
   </div>
 </template>
 
 <script>
   import Slider from '@/base/slider/slider'
+  import Scroll from '@/base/scroll/scroll'
+  import Loading from '@/base/loading/loading'
   import { getRecommend, getDiscList } from '@/api/recommend'
   import { ERR_OK } from '@/api/config'
 
   export default { 
-    components: {
-      Slider
-    },
     data() {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created() {
@@ -41,7 +57,6 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if(res.code === ERR_OK) {
-            console.log(res);
             this.recommends = res.data.slider;
           }
         })
@@ -49,14 +64,21 @@
       _getDiscList() {
         getDiscList().then((res) => {
           if(res.code === ERR_OK) {
-            console.log();
+            this.discList = res.data.list;
           }
         })
+      },
+      loadImage() {
+        if(!this.checkLoaded) {  //checkLoaded标志位，只执行一次，小trick
+          this.$refs.scroll.refresh();
+          this.checkLoaded =true;
+        }
       }
     },
-    
     components: {
-      Slider
+      Scroll,
+      Slider,
+      Loading
     }
   }
 </script>
