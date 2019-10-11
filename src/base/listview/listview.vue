b239343778a69708d6c0c47bb7e3dc92800eba3c
李阳 <11103871@vivo.xyz>
2019-10-11 14:16:24 +0800

    feat: hhh

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,13 +1,176 @@
-<template>
-  
+ <template>
+  <scroll class="listview"
+          :data="data" 
+          ref="listview" 
+          :listenScroll="listenScroll" 
+          @scroll="scroll">
+    <ul>
+      <li v-for="(group,index) in data" class="list-group" :key="index" ref="listGroup">
+        <h2 class="list-group-title">{{ group.title }}</h2>
+        <ul>
+          <li v-for="(item,indexs) in group.items" class="list-group-item" :key="indexs">
+            <img class="avatar"  v-lazy="item.avatar" />
+            <span class="name">{{ item.name }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li class="item" :data-index="index" v-for="(item, index) in shortcutList" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </scroll>
 </template>
 
 <script>
-export default {
-  
-}
+  import Scroll from '@/base/scroll/scroll'
+  import { getData } from '@/common/js/dom'
+
+  const ANCHOR_HEIGHT = 18
+
+  export default {
+    created() {
+      this.touch = {}
+      this.lsitenScroll = true
+      this.listHeight = []
+    },
+    data() {
+      return {
+        scrollY: -1,
+        currentIndex: 0
+      }
+      
+    },
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
+      }
+    },
+    methods: {
+      onShortcutTouchStart(e) {
+        let anchorIndex = getData(e.target, 'index');
+        let firstTouch = e.touches[0];
+        this.touch.y1 = firstTouch.pageY;
+        this.touch.anchorIndex = anchorIndex;
+        this._scrollTo(anchorIndex)
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0];
+        this.touch.y2 = firstTouch.pageY;
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0;  //相当于Math.floor()
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta;
+        this._scrollTo(anchorIndex)
+      },
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
+      _scrollTo (index) {
+        this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      },
+      _calculateHeight() {
+
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          
+        })
+      }
+    },
+    components: {
+      Scroll
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
+  @import "@/common/scss/variable.scss";
 
+  .listview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: $color-background;
+    .list-group {
+      padding-bottom: 30px;
+      .list-group-title {
+        height: 30px;
+        line-height: 30px;
+        padding-left: 30px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+        background: $color-highlight-background;
+      }
+      .list-group-item {
+        display: flex;
+        align-items: center;
+        padding: 20px 0 0 30px;
+        .avatar {
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+        }
+        .name {
+          margin-left: 20px;
+          color: $color-text-l;
+          font-size: $font-size-medium;
+        }
+      }
+    }
+    .list-shortcut {
+      position: absolute;
+      z-index: 30;
+      right: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 20px;
+      padding: 20px 0;
+      border-radius: 10px;
+      text-align: center;
+      background: $color-background-d;
+      font-family: Helvetica;
+      .item {
+        padding: 3px;
+        line-height: 1;
+        color: $color-text-l;
+        font-size: $font-size-small;
+        &.current {
+          color: $color-theme;
+        }
+      }
+    }
+    .list-fixed {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      .fixed-title {
+        height: 30px;
+        line-height: 30px;
+        padding-left: 20px;
+        font-size: $font-size-small;
+        color: $color-text-l;
+        background: $color-highlight-background;
+      }
+    }
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
 </style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="singer" ref="singer">
-    singer
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
   import Singer from '@/common/js/singer'
+  import ListView from '@/base/listview/listview'
   import { getSingerList } from '@/api/singer'
   import { ERR_OK } from '@/api/config'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
 
   export default {
     data() {
@@ -17,17 +21,60 @@
     },
     created() {
       this._getSingerList();
+      
     },
     methods: {
       _getSingerList() {
         getSingerList().then((res) => {
           if(res.code === ERR_OK) {
-            this.singers =  res.data.list;
+            this.singers =  this._normalizeSinger(res.data.list);
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if(index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex;
+          if(!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        let hot = [];
+        let ret = [];
+        for(let key in map) {
+          let val = map[key];
+          if(val.title.match(/[a-zA-Z]/)) {
+            ret.push(val);
+          } else if (val.title === HOT_NAME) {
+            hot.push(val);
+          }
+        }
+        ret.sort((a,b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        })
+        return hot.concat(ret);
       }
     },
     components: {
+      ListView
     }
   }
 </script>
